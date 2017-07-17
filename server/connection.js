c11d908ea8b74e66393a7e913fbaa2737d8501e8
goddessting <1766153116@qq.com>
2017-07-17 10:52:37 +0800

    modify connection.js file

--- server/connection.js
+++ server/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 let db = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: '123456',
+    password: '',
     database: 'USER',
     port: 3306
 });
